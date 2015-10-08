0ddec2685ff7e11c549aa71e50285fd9f91f5572
Larry Hwang <Larry@larry-macbook-pro.local>
2015-10-08 10:15:50 +0800

    Initial Commit

---
+++ 清房助手/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  清房助手
+//
+//  Created by Larry on 15/10/8.
+//  Copyright © 2015年 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 清房助手/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  清房助手
+//
+//  Created by Larry on 15/10/8.
+//  Copyright © 2015年 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
