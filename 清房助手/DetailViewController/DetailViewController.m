670fc1f3e1804778b9b457028c867667c5ddf85f
Larry Hwang <Larry@larry-macbook-pro.local>
2015-10-16 16:58:57 +0800

    1.图片滚动的标签已经固定并与网络数据关联 2.cell已经实现动态大小固定

--- 清房助手/DetailViewController/DetailViewController.m
+++ 清房助手/DetailViewController/DetailViewController.m
@@ -13,19 +13,22 @@
 #import "FactoryLoactionCell.h"
 #import "FlatDetailCell.h"
 #import "FlatLocationCell.h"
+#import "UIImageView+WebCache.h"
 
 
 #define  HeavyFont     [UIFont fontWithName:@"Helvetica-Bold" size:25]
-#define  ToolHeight  60
+#define  ToolHeight  60    //固定底部的大小
 
 
 @interface DetailViewController ()<UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate>
 @property (strong, nonatomic)  UITableView *detailInfoTable;
-@property (strong, nonatomic) NSArray *imagesData;
+@property (strong, nonatomic) NSMutableArray *imagesData;
 @property(nonatomic,weak)  UIButton *CountLabel;
 @property (strong, nonatomic)  UIScrollView *scrollView3;
 @property(nonatomic,strong)  NSDictionary  *FangData;
 @property(nonatomic) CGFloat CellHeight;
+@property(nonatomic,strong)  UIView  *HeaderContent;
+@property(nonatomic) NSInteger ImgTotal;
 
 
 
@@ -44,10 +47,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //底部加载
-   // UIView *footer = [[UIView alloc]initWithFrame:CGRectMake(0, self.detailInfoTable.height , ScreenWidth, ToolHeight)];
     
-   
     self.detailInfoTable = [[UITableView alloc]init];
     self.detailInfoTable.delegate = self ;
     self.detailInfoTable.dataSource = self;
@@ -56,69 +56,58 @@
     
      UIView *footer = [[UIView alloc]initWithFrame:CGRectMake(0,self.detailInfoTable.height, ScreenWidth, ToolHeight)];
      footer.backgroundColor = [UIColor redColor];
- 
     [self.view addSubview:self.detailInfoTable];
-       [self.view addSubview:footer];
-    //self.detailInfoTable.tableFooterView  = footer ;
-    
-    
-    
-    
+    [self.view addSubview:footer];
+
+    UIView *HeaderContent = [[UIView alloc]initWithFrame:CGRectMake(0, 0, ScreenWidth,ScreenHeight/4)];
+    self.HeaderContent = HeaderContent;
     self.scrollView3  = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, ScreenWidth,ScreenHeight/4)];
+    [HeaderContent addSubview:self.scrollView3];
     self.scrollView3.pagingEnabled = YES ;
     self.scrollView3.delegate = self ;
     self.scrollView3.showsHorizontalScrollIndicator  =  NO ;
-    self.imagesData = @[@"image1.jpg", @"image2.jpg", @"image3.jpg", @"image4.jpg", @"image5.jpg", @"image6.jpg"];
-    [self setupScrollViewImages];
+    self.imagesData = [NSMutableArray array];
+
+    
+    [self initCountLabel];
     AFHTTPRequestOperationManager *mgr  = [AFHTTPRequestOperationManager manager];
     
     NSString *url3 = [NSString stringWithFormat:@"http://192.168.1.38:8080/qfzsapi/fangyuan/detailsHouse.api?fenLei=0&fangyuan_id=%@",self.DisplayId];
-    //self
-    UIButton *CurrentCountLable  = [[UIButton alloc]initWithFrame:CGRectMake(ScreenWidth-50, ScreenHeight/4-50, 40, 40)];
-    self.CountLabel = CurrentCountLable ;
-    self.CountLabel.backgroundColor = [[UIColor grayColor] colorWithAlphaComponent:0.5];
-    [CurrentCountLable setTitle:@"1/6" forState:UIControlStateNormal];
     
-      //加粗选中数字
-    NSMutableAttributedString *HeavyNo = [[NSMutableAttributedString alloc]initWithString:CurrentCountLable.titleLabel.text];
-    NSRange rangeHeavyPart =  NSMakeRange(0, 2);
-    [HeavyNo addAttribute:NSFontAttributeName value:HeavyFont range:rangeHeavyPart];
-    [CurrentCountLable.titleLabel setAttributedText:HeavyNo];
-    
-    [CurrentCountLable.titleLabel setTextColor:[UIColor whiteColor]];
-    CurrentCountLable.titleLabel.textAlignment =  NSTextAlignmentCenter ;
-    CurrentCountLable.layer.cornerRadius  = 20;
-    CurrentCountLable.layer.masksToBounds = YES ;
-    [self.view addSubview:CurrentCountLable];
-    [self setupScrollViewImages];
     self.scrollView3.delegate = self;
 
     
     [mgr POST:url3
    parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
        self.FangData = responseObject[@"data"];
+       NSString *collect = self.FangData[@"tupian"];
+       NSArray *imgArray = [collect componentsSeparatedByString:@","];
+       self.ImgTotal = [imgArray count];
+       for (NSString *imgName in imgArray) {
+           NSString *ImgfullUrl = [NSString stringWithFormat:@"http://112.74.64.145/hsf/img/%@",imgName];
+           [self.imagesData addObject:ImgfullUrl];
+       }   //所有图片地址
+
+       
        CGSize size = CGSizeMake(ScreenWidth, 1000);
        NSString *context = self.FangData[@"fangyuanmiaoshu"];
        NSDictionary *attrs = @{NSFontAttributeName:[UIFont systemFontOfSize:15]};
-
        CGSize labelSize = [context boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
        self.CellHeight = labelSize.height ;
-       /*
- 
-        self.Describe.text = context ;
-        self.Describe.font = ;
-     
-        NSLog(@"仅仅高度%f",labelSize.height);
-        */
-       
-       NSLog(@"单房源信息%@",responseObject);
+
+
        [self.detailInfoTable reloadData];
+       [self viewDidLayoutSubviews];     //设置滚动视图的横向大小
+        [self setupScrollViewImages];   //设置滚动视图内图片
+
+       [self CountReset];
+       
    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
        NSLog(@"%@",error);
    }];
 
-    self.detailInfoTable.tableHeaderView = self.scrollView3;
-  
+//  self.detailInfoTable.tableHeaderView = self.scrollView3;
+    self.detailInfoTable.tableHeaderView = HeaderContent;
     self.detailInfoTable.separatorStyle = UITableViewCellSeparatorStyleNone ;
     
     
@@ -140,11 +129,14 @@
 #pragma mark - ScrollView delegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-
+    NSLog(@"%@",scrollView);
     NSInteger pageIndex = scrollView.contentOffset.x / CGRectGetWidth(scrollView.frame);
-    NSString  *nowSelected =[NSString stringWithFormat:@"%d/6",pageIndex + 1];
+    NSString  *nowSelected =[NSString stringWithFormat:@"%d/%d",pageIndex + 1,self.ImgTotal];
+    NSLog(@"哈哈%@",nowSelected);
     [self.CountLabel setTitle:nowSelected forState:UIControlStateNormal];
-    NSMutableAttributedString *HeavyNo = [[NSMutableAttributedString alloc]initWithString:self.CountLabel.titleLabel.text];
+    self.CountLabel.titleLabel.text = nowSelected ;
+    NSLog(@"嘻嘻:%@",self.CountLabel.titleLabel.text);
+    NSMutableAttributedString *HeavyNo = [[NSMutableAttributedString alloc]initWithString:nowSelected];
     NSRange rangeHeavyPart =  NSMakeRange(0, 2);
     [HeavyNo addAttribute:NSFontAttributeName value:HeavyFont range:rangeHeavyPart];
     [self.CountLabel.titleLabel setAttributedText:HeavyNo];
@@ -158,8 +150,9 @@
     [self.imagesData enumerateObjectsUsingBlock:^(NSString *imageName, NSUInteger idx, BOOL *stop) {
         UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(CGRectGetWidth(self.scrollView3.frame) * idx, 0, CGRectGetWidth(self.scrollView3.frame), CGRectGetHeight(self.scrollView3.frame))];
         imageView.contentMode = UIViewContentModeScaleAspectFill;
-        imageView.image = [UIImage imageNamed:imageName];
-        NSLog(@"一次");
+      //  imageView.image = [UIImage imageNamed:imageName];
+        NSLog(@"图片地址%@",self.imagesData[idx]);
+        [imageView sd_setImageWithURL:self.imagesData[idx] placeholderImage:nil];
         [self.scrollView3 addSubview:imageView];
     }];
     
@@ -174,33 +167,18 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath  {
     
     FactoryLoactionCell  *LocationCell  = [FactoryLoactionCell new];
-    NSLog(@"真正%@",self.FangData);
+    LocationCell.backgroundColor = [UIColor greenColor];
 
-    
-    
-
-    
-    
-    
     FlatDetailCell   *DetailCell = [FlatDetailCell new];
-    
-    
-    
-    
-    
-    
+
     
     DescribeCell  *DescribieCell = [DescribeCell new];
-    
-    
-    
     
 
     
    // UILabel
     
     LocationCell  =  [[[NSBundle mainBundle]loadNibNamed:@"FactoryLoactionCell" owner:nil options:nil] firstObject];
- //   DescribieCell = [[[NSBundle mainBundle]loadNibNamed:@"DescribeCell" owner:nil options:nil] firstObject];
     DetailCell = [[[NSBundle mainBundle]loadNibNamed:@"FlatDetailCell" owner:nil options:nil] firstObject];
     
     
@@ -226,7 +204,7 @@
     }else {
         DescribieCell.Describe.numberOfLines = 0;
              DescribieCell.Describe.backgroundColor = [UIColor blueColor];
-    //  DescribieCell.Describe.text = @"李琦参加好声音前就有过不少表演经历";
+
        [DescribieCell setDescribeText:self.FangData[@"fangyuanmiaoshu"]];
         NSLog(@"label高度%f",DescribieCell.Describe.frame.size.height);
      //   [DescribieCell.Describe setFrame:CGRectMake(0, 0, ScreenWidth, 100)];
@@ -249,13 +227,36 @@
         return 200;
     }
   else {
-//      DescribeCell *cell = [self.detailInfoTable cellForRowAtIndexPath:indexPath];
-//      NSLog(@"%f",cell.height);
-////       return cell.frame.size.height;
-     return self.CellHeight;
+     return self.CellHeight + 10 ;
     }
 }
 
 
+#pragma mark -计数器的初始化
+-(void)initCountLabel {
+    UIButton *CurrentCountLable  = [[UIButton alloc]initWithFrame:CGRectMake(ScreenWidth-50, ScreenHeight/4-50, 40, 40)];
+    self.CountLabel = CurrentCountLable ;
+ 
+    self.CountLabel.backgroundColor = [[UIColor grayColor] colorWithAlphaComponent:0.5];
+    [CurrentCountLable setTitle:[NSString stringWithFormat:@"1/1"] forState:UIControlStateNormal];
+    
+    //加粗选中数字
+    NSMutableAttributedString *HeavyNo = [[NSMutableAttributedString alloc]initWithString:CurrentCountLable.titleLabel.text];
+    NSRange rangeHeavyPart =  NSMakeRange(0, 2);
+    [HeavyNo addAttribute:NSFontAttributeName value:HeavyFont range:rangeHeavyPart];
+    [CurrentCountLable.titleLabel setAttributedText:HeavyNo];
+    [CurrentCountLable.titleLabel setTextColor:[UIColor whiteColor]];
+    CurrentCountLable.titleLabel.textAlignment =  NSTextAlignmentCenter ;
+    CurrentCountLable.layer.cornerRadius  = 20;
+    CurrentCountLable.layer.masksToBounds = YES ;
+    [self.HeaderContent addSubview: self.CountLabel];
+}
+
+
+#pragma mark -计数器重置
+-(void)CountReset {
+    [self.CountLabel setTitle:[NSString stringWithFormat:@"1/%ld",(long)self.ImgTotal] forState:UIControlStateNormal];
+    [self scrollViewDidScroll:self.scrollView3];
+}
 
 @end
