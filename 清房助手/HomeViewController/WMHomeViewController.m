0747d4997bb20f67b4163b91a8e49bbaf4313a98
Larry Hwang <Larry@larry-macbook-pro.local>
2015-10-20 15:20:45 +0800

    顶部TabBar切换功能完成

--- 清房助手/HomeViewController/WMHomeViewController.m
+++ 清房助手/HomeViewController/WMHomeViewController.m
@@ -18,11 +18,20 @@
 #import "SCNavTabBarController.h"
 
 
+#define SingleBtnWidth   ScreenWidth/2
+#define TopTabBarHeight  32
 
-
-@interface WMHomeViewController()
+@interface WMHomeViewController() {
+#pragma mark 优化成结构体
+    
+    BOOL            _isSaleStatus;
+    NSMutableArray  *_TabBarBtns;
+    UIView          *_bottomLine;
+    
+}
 
 @property(nonatomic,strong)  NSArray  *DataArr;
+@property(nonatomic,strong)  AFHTTPRequestOperationManager  *shareMgr;
 
 @end
 
@@ -32,6 +41,7 @@
 -(NSArray *)DataArr {
     if (_DataArr ==nil) {
         _DataArr = [NSArray array];
+      
     }
     return _DataArr;
 }
@@ -39,7 +49,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    _TabBarBtns = [NSMutableArray arrayWithCapacity:2];
     
 #warning 整合部分
     
@@ -51,46 +61,13 @@
     UIViewController *twoViewController = [[UIViewController alloc] init];
     twoViewController.title = @"出售";
     twoViewController.view.backgroundColor = [UIColor blueColor];
-    
-    
-    self.tableView.backgroundColor = [UIColor redColor];
-    
-    
-    
 
-    
-    
-//    SCNavTabBarController *navTabBarController = [[SCNavTabBarController alloc] init];
-//    navTabBarController.subViewControllers = @[self, twoViewController];
-//    navTabBarController.showArrowButton = NO;
-//  // [navTabBarController addParentController:self];
-//    [self.view addSubview:navTabBarController.view];
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     self.tableView.delegate = self;
     AFHTTPRequestOperationManager *mgr  = [AFHTTPRequestOperationManager manager];
+    self.shareMgr = mgr ;
     
-    NSString *url3=@"http://192.168.1.38:8080/qfzsapi/fangyuan/rentalOrBuyHouseSearch.api?weiTuoDate=0&sum=10&fangxiang=refresh&zuShou=0";
-    [mgr POST:url3
-   parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-       NSLog(@"%@",responseObject);
-       NSArray *DataArra = responseObject[@"data"];
-       self.DataArr =DataArra;
-       [self.tableView reloadData];
-        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"%@",error);
-    }];
-    
+    [self SalesTableLoad];
     
     self.navigationItem.backBarButtonItem.title = @"返回";
     UIButton *IconBtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -119,16 +96,94 @@
 
     
 #warning 测试部分
-    //    SCNavTabBarController *navTabBarController = [[SCNavTabBarController alloc] init];
-    //    navTabBarController.subViewControllers = @[self, twoViewController];
-    //    navTabBarController.showArrowButton = NO;
-    //  // [navTabBarController addParentController:self];
-    //    [self.view addSubview:navTabBarController.view];
-    SCNavTabBarController *navTabBarController = [[SCNavTabBarController alloc]init];
-  //  navTabBarController
-   // TabBar.backgroundColor = [UIColor blueColor];
-  //  [self.view addSubview:navTabBarController.view];
+    UIView *TabBarContentView = [[UIView alloc]initWithFrame:CGRectMake(0, 64, ScreenWidth, TopTabBarHeight)];
+    UIButton *Sales  = [UIButton buttonWithType:UIButtonTypeCustom];
+    Sales.tag = 0 ;
+    [Sales setTitle:@"出售" forState:UIControlStateNormal];
+    [Sales setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [Sales setTitleColor:DeafaultColor forState:UIControlStateSelected];
+    Sales.selected = YES;
+    [Sales setFrame:CGRectMake(0, 0, SingleBtnWidth ,TopTabBarHeight )];
+    [Sales addTarget:self action:@selector(TabBarBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [_TabBarBtns addObject:Sales];
+    
+    
+    UIView *HorizontalLine = [[UIView alloc]initWithFrame:CGRectMake(0, TopTabBarHeight -2, ScreenWidth, 2)];
+    HorizontalLine.backgroundColor = [UIColor grayColor];
+    
+    UIView *VerticalLine = [[UIView alloc]initWithFrame:CGRectMake(ScreenWidth/2,  5, 2, TopTabBarHeight - 10)];
+    VerticalLine.backgroundColor = [UIColor grayColor];
+    
+    UIView *HilghtLine = [[UIView alloc]initWithFrame:CGRectMake(SingleBtnWidth/4, TopTabBarHeight-2, SingleBtnWidth/2, 2)];
+    HilghtLine.backgroundColor = DeafaultColor ;
+    _bottomLine = HilghtLine;
+    
+    
+    UIButton *Rent  = [UIButton buttonWithType:UIButtonTypeCustom];
+    Rent.tag  = 1 ;
+    [Rent setTitle:@"出租" forState:UIControlStateNormal];
+    [Rent setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [Rent setTitleColor:DeafaultColor forState:UIControlStateSelected];
+    [Rent setFrame:CGRectMake(SingleBtnWidth, 0, SingleBtnWidth  ,TopTabBarHeight )];
+    [Rent addTarget:self action:@selector(TabBarBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [_TabBarBtns addObject:Rent];
+    
+    [TabBarContentView addSubview:Sales];
+    [TabBarContentView addSubview:Rent];
+    [TabBarContentView addSubview:HorizontalLine];
+    [TabBarContentView addSubview:VerticalLine];
+    [TabBarContentView addSubview:HilghtLine];
+    TabBarContentView.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:TabBarContentView];
 
+}
+
+
+-(void)SalesTableLoad {
+    NSString *url3=@"http://192.168.1.38:8080/qfzsapi/fangyuan/rentalOrBuyHouseSearch.api?weiTuoDate=0&sum=10&fangxiang=refresh&zuShou=0";
+    [self.shareMgr POST:url3
+   parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+       NSLog(@"%@",responseObject);
+       NSArray *DataArra = responseObject[@"data"];
+       self.DataArr =DataArra;
+       [self.tableView reloadData];
+   } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+       NSLog(@"%@",error);
+   }];
+
+}
+
+
+-(void)RenTableLoad {
+        NSString *url3=@"http://192.168.1.38:8080/qfzsapi/fangyuan/rentalOrBuyHouseSearch.api?weiTuoDate=0&sum=10&fangxiang=refresh&zuShou=1";
+        [self.shareMgr POST:url3
+                 parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                     NSLog(@"%@",responseObject);
+                     NSArray *DataArra = responseObject[@"data"];
+                     self.DataArr =DataArra;
+                     [self.tableView reloadData];
+                 } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                     NSLog(@"%@",error);
+                 }];
+        
+
+}
+
+-(void)TabBarBtnClick :(UIButton *)btn {
+    btn.selected = YES ;
+    UIButton *anotherBtn = btn.tag ?  _TabBarBtns[0]: _TabBarBtns[1] ;
+    [UIView animateWithDuration:0.3f animations:^{
+        anotherBtn.selected = NO;
+        [_bottomLine setFrame:CGRectMake(btn.frame.origin.x + SingleBtnWidth/4, TopTabBarHeight -2 , SingleBtnWidth/2, 2)];
+    }];
+    if (btn.tag == 0) {  //出售
+        [self SalesTableLoad];
+        NSLog(@"出售");
+    }else {
+        [self RenTableLoad];
+        NSLog(@"出租");
+    }
 }
 
 - (void)clicked {
